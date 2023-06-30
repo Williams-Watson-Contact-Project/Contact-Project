f6c410659a86c76bf7ff3e447f96c0ef91c34c22
Johann Williams <johann.williams01@gmail.com>
2023-06-30 14:18:24 -0500

    updated testPhone to test new and updated methods/constructors

--- src/testPhone.java
+++ src/testPhone.java
@@ -4,17 +4,18 @@ public class testPhone {
 
     public static void main(String[] args) {
 
-        Input i = new Input();
         Phone p1 = new Phone();
-        Phone p2 = new Phone("512-512-5123");
+        Phone p2 = new Phone("5125125123");
+        Phone p3 = new Phone("512-512-51");
 
         System.out.println(p1.getNumber());
-        p1.setNumber("512-512-5123");
+        p1.setNumber("5125123");
         System.out.println(p1.getNumber());
 
         System.out.println(p2.getNumber());
         p2.setNumber("5123456789");
         System.out.println(p2.getNumber());
 
+        System.out.println("p3.getNumber() = " + p3.getNumber());
     }
 }
