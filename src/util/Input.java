63c57c409cbe55b42650fbfe4268ce926b98726c
Johann Williams <johann.williams01@gmail.com>
2023-06-30 17:04:24 -0500

    updated input to take in and check for proper integers, integers in a range, and boolean for yes or no

--- src/util/Input.java
+++ src/util/Input.java
@@ -12,4 +12,32 @@ public class Input {
     public String getString() {
         return scanner.nextLine();
     }
+
+    public boolean yesNo(){
+        System.out.println("Enter Yes/Y or No/N");
+        String yNStr = this.scanner.nextLine();
+        return yNStr.equalsIgnoreCase("yes") || yNStr.equalsIgnoreCase("y");
+    }
+
+    public int getInt(int min, int max){
+        int userInt = 0;
+        do {
+            System.out.printf("Enter a number between %d and %d\n", min, max);
+            userInt = getInt();
+        }while(userInt > max || userInt < min);
+
+        return userInt;
+    }
+
+    public int getInt(){
+        int val = -1;
+        String s = getString();
+        try{
+            val = Integer.valueOf(s);
+        }catch(NumberFormatException e){
+            System.err.println("Input is not a valid integer!\nTry Again!\n" + e);
+            val = getInt();
+        }
+        return val;
+    }
 }
