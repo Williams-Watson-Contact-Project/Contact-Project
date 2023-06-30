8c0ea280cf0be97c5a838b7ad8b7f0fd249bd9b2
Johann Williams <johann.williams01@gmail.com>
2023-06-30 14:17:50 -0500

    added comments, updated phone to format numbers with '-' if they are valid numbers, updated constructor and setNumber to check format and update number with'-'s if needed.

--- src/Phone.java
+++ src/Phone.java
@@ -6,33 +6,66 @@ public class Phone {
     private String number;
     private final String defaultNumber = "000-000-0000";
 
+    /**
+     * default constructor
+     * set this.number to as default number.
+     */
     public Phone(){
         this.number = defaultNumber;
     }
 
+    /**
+     * Constructor, sets number to the number passed in
+     * so long as it is of a proper format. if not set
+     * number to the default number.
+     * @param number
+     */
     public Phone(String number) {
-        this.number = number;
+        if(checkNumberFormat(number)){
+            if(number.contains("-")){
+                this.number = number;
+            }else{
+                this.number = formatNumber(number);
+            }
+        } else {
+            this.number = defaultNumber;
+        }
     }
 
+    /**
+     * return this.number
+     * @return
+     */
     public String getNumber() {
         return number;
     }
 
+    /**
+     * So long as the number passed in is of the correct format
+     * set this.number to the number passed in, in the format of
+     * xxx-xxxx or xxx-xxx-xxxx.
+     * @param number
+     */
     public void setNumber(String number) {
         if(checkNumberFormat(number)){
-            this.number = number;
+            if(number.contains("-")){
+                this.number = number;
+            }else{
+                this.number = formatNumber(number);
+            }
         }else {
             System.out.println("Invalid number");
         }
     }
 
-    public boolean checkNumberFormat(String number){
+    /**
+     * Check to see if the number passed in is of a valid format.
+     * Valid numbers ar xxxxxxx | xxxxxxxxxx | xxx-xxxx | xxx-xxx-xxxx
+     * @param number
+     * @return
+     */
+    private boolean checkNumberFormat(String number){
         boolean correctFormat = false;
-
-        String temp1 = "1234567";
-        String temp2 = "123-4567";
-        String temp3 = "8901234567";
-        String temp4 = "890-123-4567";
 
         if(number.contains("-")){
             if(Pattern.matches("^[2-9]\\d{2}-\\d{3}-\\d{4}$|\\d{3}-\\d{4}$", number)){
@@ -45,5 +78,22 @@ public class Phone {
         }
 
         return correctFormat;
+    }
+
+    /**
+     * Takes in a phone number as a string and converts it into the correct
+     * format of xxx-xxx-xxxx or xxx-xxxx;
+     * @param number
+     * @return
+     */
+    private String formatNumber(String number){
+        StringBuilder sb = new StringBuilder(number);
+        switch (number.length()){
+            case 10:
+                sb.insert(6, '-');
+            case 7:
+                sb.insert(3,'-');
+        }
+        return sb.toString();
     }
 }
