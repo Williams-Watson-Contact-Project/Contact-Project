e97de1aa894874f8df179f4e46f57571a652a44e
Tiffani <tiffani.marie.watson@gmail.com>
2023-06-30 16:09:45 -0500

    created to test

--- src/testPerson.java
+++ src/testPerson.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class TestPerson {
+public class testPerson {
+    public static void main(String[] args) {
+
+      Person person1 = new Person("barney","America", "723-435-2333");
+        System.out.println("person1.getContact() = " + person1.getContact());
+        person1.setNumber("324-544-2345");
+        System.out.println("person1.getContact() = " + person1.getContact());
+    }
 }
