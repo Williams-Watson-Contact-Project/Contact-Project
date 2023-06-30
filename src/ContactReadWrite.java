8709410b20b95d334478462b47cc83618599b7cb
Johann Williams <johann.williams01@gmail.com>
2023-06-30 17:06:58 -0500

    created a class for reading and writing contact info. variables/constructor/writeTo/ReadAll/ReadSpecificContactByName/checkifContactExits all work. Delete Contact started.

---
+++ src/ContactReadWrite.java
@@ -0,0 +1,102 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
+
+public class ContactReadWrite {
+    private final String directory;
+    private final String filename;
+
+    public ContactReadWrite(){
+        this.directory = "data";
+        this.filename = "contacts.txt";
+        try {
+            this.checkAndSetFileAndDirector();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void checkAndSetFileAndDirector() throws IOException {
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
+
+        if (Files.notExists(dataDirectory)) {
+            Files.createDirectories(dataDirectory);
+        }
+
+        if (! Files.exists(dataFile)) {
+            Files.createFile(dataFile);
+        }
+    }
+
+    public void displayAllContacts() throws IOException {
+        Path contactsPath = Paths.get(directory, filename);
+        List<String> contactList = Files.readAllLines(contactsPath);
+
+        System.out.printf("%3s| %-15s | %12s |%n"," ", "Name", "Number");
+
+        for (int i = 0; i < contactList.size(); i += 1) {
+            System.out.println((i + 1) + ": " + contactList.get(i));
+        }
+    }
+
+    public boolean checkIfContactExists(String fullName) throws IOException {
+        boolean nameExits = false;
+        Path contactsPath = Paths.get(directory, filename);
+        List<String> contactList = Files.readAllLines(contactsPath);
+
+        for(String listString : contactList){
+            if(listString.contains(fullName)){
+                nameExits = true;
+                break;
+            }
+        }
+
+        return nameExits;
+    }
+
+    public void displayContactByName(String name) throws IOException {
+        if(name.contains(" ")){
+            Path contactsPath = Paths.get(directory, filename);
+            List<String> contactList = Files.readAllLines(contactsPath);
+
+            for(String listString : contactList){
+                if(listString.contains(name)){
+                    System.out.println(listString);
+                    break;
+                }
+            }
+        }
+    }
+
+    public void deleteContactByName(String name) throws IOException {
+        if(name.contains(" ")){
+            Path contactsPath = Paths.get(directory, filename);
+            List<String> contactList = Files.readAllLines(contactsPath);
+            List<String> newContactList = Files.readAllLines(contactsPath);
+
+            for(String listString : contactList){
+                if(!listString.contains(name)){
+                    newContactList.add(listString);
+                }
+            }
+            Files.write(Paths.get(directory, filename), newContactList);
+        }
+    }
+
+    public void writeNewContactInfo(Person newPerson) throws IOException {
+        if(!checkIfContactExists(newPerson.getFullName())){
+            Files.write(
+                    Paths.get(directory, filename),
+                    Arrays.asList(newPerson.getContact()), // list with one item
+                    StandardOpenOption.APPEND
+            );
+        } else {
+            System.out.println("Contact already exists!");
+        }
+    }
+}
