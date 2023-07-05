0884351c3c0acfe0fb059140a84b8fc581ab8111
Johann Williams <johann.williams01@gmail.com>
2023-07-05 15:14:01 -0500

    added comments and updated constructor comment for Contact class.

--- src/Contact.java
+++ src/Contact.java
@@ -5,35 +5,64 @@ public class Contact {
 
     private Phone phone;
 
-    //Constructor of a person with a name and last number with a number that passes it in
+    /**
+     * Constructor of a contact with a first and last name and number that passes it in
+     */
     public Contact(String firstName, String lastName, String number) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.phone = new Phone(number);
     }
 
+    /**
+     * returns Contact's first name
+     * @return
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * returns Contact's last name
+     * @return
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * returns Contact's Phone number by calling
+     * the Phone's getNumber() method.
+     * @return
+     */
     public String getPhone() {
         return phone.getNumber();
     }
 
+    /**
+     * returns the Contact's Full name as
+     * first last.
+     * @return
+     */
     public String getFullName(){
         return firstName + " " + lastName;
     }
 
-
-
+    /**
+     * Returns the Full Contact information formated as
+     * | full name | phone number |
+     * @return
+     */
     public String getContact(){
         return String.format("| %-15s | %12s |", getFullName() , getPhone());
     }
 
+    /**
+     * updates Contact's phone number so long as
+     * the number being passed in is a valid number.
+     * validation checks happen in Phone class.
+     * @param phoneNumber
+     */
     public void setNumber(String phoneNumber){
         this.phone.setNumber(phoneNumber);
     }
