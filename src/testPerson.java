e5c24b7e31a88e8966ed9e11b5f268318d265bd6
Johann Williams <johann.williams01@gmail.com>
2023-07-05 15:02:05 -0500

    deleted unused java files for testing Phone class and Person/Contact class

--- src/testPerson.java
+++
@@ -1,9 +0,0 @@
-public class testPerson {
-    public static void main(String[] args) {
-
-      Contact contact1 = new Contact("barney","America", "723-435-2333");
-        System.out.println("contact1.getContact() = " + contact1.getContact());
-        contact1.setNumber("324-544-2345");
-        System.out.println("contact1.getContact() = " + contact1.getContact());
-    }
-}
