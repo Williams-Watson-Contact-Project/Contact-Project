42b79a37eb41dc0f70798a210b1f20f5844e4822
Johann Williams <johann.williams01@gmail.com>
2023-07-05 10:17:03 -0500

    added comments to methods.

--- src/util/Input.java
+++ src/util/Input.java
@@ -5,20 +5,41 @@ import java.util.Scanner;
 public class Input {
     private final Scanner scanner;
 
+    /**
+     * Default constructor, initializes a new Scanner.
+     */
     public Input() {
         this.scanner = new Scanner(System.in);
     }
 
+    /**
+     * Takes in a user input as a string.
+     * @return
+     */
     public String getString() {
         return scanner.nextLine();
     }
 
+    /**
+     * checks for a yes or no condition and returns
+     * true if user enters yes or y ignoring case else it is
+     * false.
+     * @return
+     */
     public boolean yesNo(){
         System.out.println("Enter Yes/Y or No/N");
         String yNStr = this.scanner.nextLine();
         return yNStr.equalsIgnoreCase("yes") || yNStr.equalsIgnoreCase("y");
     }
 
+    /**
+     * Ask user for an integer within a set range. If
+     * integer is out of range, ask again. Check for
+     * a proper integer input in getInput() method.
+     * @param min
+     * @param max
+     * @return
+     */
     public int getInt(int min, int max){
         int userInt = 0;
         do {
@@ -29,6 +50,11 @@ public class Input {
         return userInt;
     }
 
+    /**
+     * Checks for a proper integer input. If one is not,
+     * throw a catch and try again.
+     * @return
+     */
     public int getInt(){
         int val = -1;
         String s = getString();
